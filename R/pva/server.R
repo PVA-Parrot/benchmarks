f74ca94e2b042dfc1c44fb5ec63799032297fecd
Nils Blum-Oeste <nils.blum-oeste@wooga.net>
2016-10-23 19:44:18 +0200

    Add input value rendering and normalization

--- R/pva/server.R
+++ R/pva/server.R
@@ -1,22 +1,61 @@
 library(shiny)
 
-calculatepca <- function(input) {
+csvdata <- function(input) {
   upload <- input$fileupload
-  if (is.null(upload))
+  read.csv(upload$datapath, header = input$hasHeaders)
+}
+
+normalize <- function(data) {
+  mins <- sapply(data, min)
+  maxs <- sapply(data, max)
+  spread <- maxs - mins
+  t((t(data) - mins) / spread)
+}
+
+calculatepca <- function(input) {
+  if (is.null(input$fileupload))
     return(NULL)
   
-  data <- read.csv(upload$datapath, header = input$hasHeaders)
+  data <- csvdata(input)
   
   # remove first column if it has sample names
   if (input$hasRowNames)
     data <- data[c(-1)]
   
-  # TODO: normalize data
+  normalized <- normalize(data)
   
-  prcomp(data)
+  pca <- prcomp(normalized, center = F, scale = F)
+  
+  # PVA software uses n as a denominator in SD, we need to correct for this
+  pca$sdev <- pca$sdev * (1-1/length(normalized))
+  
+  pca
 }
 
+
+calculateeigen <- function(input) {
+  if (is.null(input$fileupload))
+    return(NULL)
+  
+  data <- csvdata(input)
+  
+  # remove first column if it has sample names
+  if (input$hasRowNames)
+    data <- data[c(-1)]
+  
+  normalized <- normalize(data)
+  
+  eigen(cov(normalized))
+  }
+
 shinyServer(function(input, output) {
+  
+  output$inputTable <- renderDataTable({
+    if (is.null(input$fileupload))
+      return(NULL)
+    csvdata(input)
+  })
+  
   output$stdevTable <- renderTable({
     sdev <- calculatepca(input)$sdev
     if (is.null(sdev))
@@ -33,12 +72,16 @@ shinyServer(function(input, output) {
   
   output$eigenvectorsTable <- renderDataTable({
     calculatepca(input)$rotation
-  },
-  options = list(searching = F,
-                 ordering = F,
-                 pageLength = 10,
-                 scrollX = T))
+    #data.frame(calculateeigen(input)$vectors)
+    }
+    # ,
+    # options = list(searching = F,
+    #                ordering = F,
+    #                pageLength = 10,
+    #                scrollX = T)
+    )
   
+  outputOptions(output, 'inputTable', suspendWhenHidden = FALSE)
   outputOptions(output, 'eigenvectorsTable', suspendWhenHidden = FALSE)
   outputOptions(output, 'stdevTable', suspendWhenHidden = FALSE)
   outputOptions(output, 'eigenvaluesTable', suspendWhenHidden = FALSE)
--- R/pva/ui.R
+++ R/pva/ui.R
@@ -14,18 +14,26 @@ shinyUI(fluidPage(
       ),
       checkboxInput("hasHeaders", "First row are table headers", value = TRUE),
       checkboxInput("hasRowNames", "First row are sample names", value = TRUE)
-      
     ),
+    
+    conditionalPanel(
+      condition = "output.inputTable",
+      tags$h2("Raw Input Data"),
+      dataTableOutput("inputTable")
+    ),
+    
     conditionalPanel(
       condition = "output.stdevTable",
       tags$h2("Standard Deviations"),
       tableOutput("stdevTable")
     ),
+    
     conditionalPanel(
       condition = "output.eigenvaluesTable",
       tags$h2("Eigenvalues"),
       tableOutput("eigenvaluesTable")
     ),
+    
     conditionalPanel(
       condition = "output.eigenvectorsTable",
       tags$h2("Eigenvectors"),
